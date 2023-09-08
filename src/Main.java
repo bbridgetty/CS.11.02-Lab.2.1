b52cbaedcba65c8ddefbb96f1612d2d0c865c117
bbridgetty <bridgett2026@student.cis.edu.hk>
2023-09-08 09:50:57 +0800

    Lab 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,19 +7,93 @@ public class Main {
     }
 
     // 1. add
-    
-    // 2. add
-
-    // 3. morningGreeting
-
-    // 4. afternoonGreeting
-
-    // 5. triple
-
-    // 6. half
-
-    // 7. roundPositiveValueToNearestInteger
-
-    // 8. roundNegativeValueToNearestInteger
-
+    public class Main {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static main(String[] args) {
+        int result = add(3, 8);
+        System.out.println(result);
+    }
 }
+    // 2. add
+    public class Main {
+    }
+    public static int add(int a, int b, int c, int d) {
+        int =add(a, b);
+        int =add(sum1, c);
+        int =add(sum2, d);
+        return sum3;
+    }
+    // 3. morningGreeting
+    public class Main {
+        public static void main(String) {
+            String greeting = morningGreeting("Toby Fox");
+            System.out.println(greeting);
+        }
+
+        public static String morningGreeting(String name) {
+            return "早上好, " + name + "!";
+        }
+    // 4. afternoonGreeting
+    public class Main {
+        public static void main(String) {
+            String greeting = afternoonGreeting("Mac Miller");
+            System.out.println(greeting);
+        }
+
+        public static String afternoonGreeting(String name) {
+            return "下午好, " + name + "!";
+        }
+    }
+    // 5. triple
+    public class Main {
+        public static void main(String) {
+            String result = ("oohbaby");
+            System.out.println(result);
+            System.out.println(result);
+            System.out.println(result);
+        }
+
+        public static String (String str) {
+            return str + str + str;
+        }
+    }
+    // 6. half
+    public class Main {
+        public static void main() {
+            double result1 = half(8);
+            System.out.println(result1);
+
+            double result2 = half(17);
+            System.out.println(result2);
+        }
+
+        public static double half(int number) {
+            return number / 2.0;
+        }
+    }
+    // 7. roundPositiveValueToNearestInteger
+    public class Main {
+        public static void main(String) {
+            double value = 8.5;
+            int roundedValue = roundPositiveValueToNearestInteger(int);
+            System.out.println(roundedValue);
+        }
+
+        public static int roundPositiveValueToNearestInteger(double value) {
+            return (int) (value + 0.5);
+        }
+    }
+    // 8. roundNegativeValueToNearestInteger
+    public class Main {
+        public static void main(String) {
+            double value = -8.5;
+            int roundedValue = roundNegativeValueToNearestInteger(int);
+            System.out.println(roundedValue);
+        }
+
+        public static int roundNegativeValueToNearestInteger(double value) {
+            return (int) (value - 0.5);
+        }
+    }
